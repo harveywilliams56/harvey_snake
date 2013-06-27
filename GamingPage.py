bafb5e786e17006376768da93483807250fcad02
James Williams <j@Jamess-MacBook.local>
2013-06-27 20:07:45 +0100

    Got the Speed selection page working
    Fixed the bug where we were trying to print a string off the screen
    Made a working version of the speed select screen
    Got 'q' to go back to the speed screen or exit whilst in the speed screen
    Added nessasery constructor parameters to GamingPage
    Replaced some tabs with spaces

--- GamingPage.py
+++ GamingPage.py
@@ -8,121 +8,122 @@ from Egg import *
 from Player import *
 
 class GamingPage:
-	def __init__(self, screen):
-		self.screen = screen
-		self.draw_area = DrawArea(screen)
-		self.x = self.draw_area.width / 4
-		self.y = self.draw_area.height / 2
-		self.d = 3
-		self.l = 5
-		self.players = []
-		self.map0 = {	ord('w') : 3,
-				ord('a') : 4,
-				ord('s') : 1,
-				ord('d') : 2}
-		self.map1 = {	curses.KEY_UP : 3,
-				curses.KEY_LEFT : 4,
-				curses.KEY_DOWN : 1,
-				curses.KEY_RIGHT : 2}
-		self.player0 = Player(self.x,self.y,self.d, self.l, self.map0,self.draw_area,True)
-		self.player1 = Player(9, 9, 1, 5, self.map1, self.draw_area,False)
-		self.pause = 1000
-		self.player0.add_to_obstacles(self.player0.snake)
-		self.player0.add_to_obstacles(self.player1.snake)
-		self.player1.add_to_obstacles(self.player0.snake)
-		self.player1.add_to_obstacles(self.player1.snake)
-		self.egg = Egg(self.draw_area, 9, 9)
-		self.player0.add_to_food(self.egg)
-		self.player1.add_to_food(self.egg)
-		self.exit_key = ord('q')
-		self.controls = "comp."
-		self.running = True
-		self.turn = 0
-	#Get list of keys since last gameloop
-	def key_list(self):
-		key_list = []
-		switch = True
-		while switch:
-			key = self.screen.getch()
-			if key == curses.ERR:
-				switch = False
-			else:
-				key_list += [key]
-		return key_list
+   def __init__(self, screen, snake_sleep, quit_page):
+      self.screen = screen
+      self.snake_sleep = snake_sleep
+      self.quit_page = quit_page
+      self.draw_area = DrawArea(screen)
+      self.x = self.draw_area.width / 4
+      self.y = self.draw_area.height / 2
+      self.d = 3
+      self.l = 5
+      self.players = []
+      self.map0 = {  ord('w') : 3,
+            ord('a') : 4,
+            ord('s') : 1,
+            ord('d') : 2}
+      self.map1 = {  curses.KEY_UP : 3,
+            curses.KEY_LEFT : 4,
+            curses.KEY_DOWN : 1,
+            curses.KEY_RIGHT : 2}
+      self.player0 = Player(self.x,self.y,self.d, self.l, self.map0,self.draw_area,True)
+      self.player1 = Player(9, 9, 1, 5, self.map1, self.draw_area,False)
+      self.pause = 1000
+      self.player0.add_to_obstacles(self.player0.snake)
+      self.player0.add_to_obstacles(self.player1.snake)
+      self.player1.add_to_obstacles(self.player0.snake)
+      self.player1.add_to_obstacles(self.player1.snake)
+      self.egg = Egg(self.draw_area, 9, 9)
+      self.player0.add_to_food(self.egg)
+      self.player1.add_to_food(self.egg)
+      self.exit_key = ord('q')
+      self.controls = "comp."
+      self.running = True
+      self.turn = 0
+   #Get list of keys since last gameloop
+   def key_list(self):
+      key_list = []
+      switch = True
+      while switch:
+         key = self.screen.getch()
+         if key == curses.ERR:
+            switch = False
+         else:
+            key_list += [key]
+      return key_list
 
-	def AI(self):
-		direction = self.player0.snake.direction()
-		egg_point_x = self.egg.position_x()
-		snake_point_x = self.player0.snake.position_x()
-		egg_point_y = self.egg.position_y()
-		snake_point_y = self.player0.snake.position_y()
-		screen_width = self.draw_area.width
-		wall_right = screen_width - snake_point_x + egg_point_x
-		plain_left = snake_point_x - egg_point_x
-		wall_left = snake_point_x + screen_width - egg_point_x
-		plain_right = egg_point_x - snake_point_x
-		screen_height = self.draw_area.height
-		wall_up = screen_height - snake_point_y + egg_point_y
-		plain_down = snake_point_y - egg_point_y
-		wall_down = snake_point_y + screen_height - egg_point_y
-		plain_up = egg_point_y - snake_point_y
-		if direction == 1 or 3:
-			if snake_point_x < egg_point_x:
-				if plain_right < wall_left:
-					return 2
-				else:
-					return 4
-			if snake_point_x > egg_point_x:
-				if plain_left < wall_right:
-					return 4
-				else:
-					return 2
-		if direction == 2 or 4:
-			if snake_point_y < egg_point_y:
-				if plain_up < wall_down:
-					return 3
-				else:
-					return 1
-			if snake_point_y > egg_point_y:
-				if plain_down < wall_up:
-					return 1
-				else:
-					return 3
-		return direction
-	def draw_loop(self):
-		# Make getch() Non-Blocking
-		self.screen.nodelay(1)
-		while self.running:
+   def AI(self):
+      direction = self.player0.snake.direction()
+      egg_point_x = self.egg.position_x()
+      snake_point_x = self.player0.snake.position_x()
+      egg_point_y = self.egg.position_y()
+      snake_point_y = self.player0.snake.position_y()
+      screen_width = self.draw_area.width
+      wall_right = screen_width - snake_point_x + egg_point_x
+      plain_left = snake_point_x - egg_point_x
+      wall_left = snake_point_x + screen_width - egg_point_x
+      plain_right = egg_point_x - snake_point_x
+      screen_height = self.draw_area.height
+      wall_up = screen_height - snake_point_y + egg_point_y
+      plain_down = snake_point_y - egg_point_y
+      wall_down = snake_point_y + screen_height - egg_point_y
+      plain_up = egg_point_y - snake_point_y
+      if direction == 1 or 3:
+         if snake_point_x < egg_point_x:
+            if plain_right < wall_left:
+               return 2
+            else:
+               return 4
+         if snake_point_x > egg_point_x:
+            if plain_left < wall_right:
+               return 4
+            else:
+               return 2
+      if direction == 2 or 4:
+         if snake_point_y < egg_point_y:
+            if plain_up < wall_down:
+               return 3
+            else:
+               return 1
+         if snake_point_y > egg_point_y:
+            if plain_down < wall_up:
+               return 1
+            else:
+               return 3
+      return direction
+   def draw_loop(self):
 
-			# Check for user input
-			keys = self.key_list()
-			for key in keys:
-				if key == self.exit_key:
-					self.running = False
-				if self.controls == "human":
-					if key == ord("p"):
-						self.controls = "comp."
-				if self.controls == "comp.":
-					if key in self.map0:
-						self.controls = "human"
-			if self.controls == "human":
-				dir = self.player0.key_decode(keys)
-			else:
-				dir = self.AI()
-			self.player0.snake.change_direction(dir)
-			dir = self.player1.key_decode(keys)
-			self.player1.snake.change_direction(dir)
+      while self.running:
 
-			# redraw the screen
-			self.draw_area.clear()
+         # Check for user input
+         keys = self.key_list()
+         for key in keys:
+            if key == self.exit_key:
+               self.running = False
+            if self.controls == "human":
+               if key == ord("p"):
+                  self.controls = "comp."
+            if self.controls == "comp.":
+               if key in self.map0:
+                  self.controls = "human"
+         if self.controls == "human":
+            dir = self.player0.key_decode(keys)
+         else:
+            dir = self.AI()
+         self.player0.snake.change_direction(dir)
+         dir = self.player1.key_decode(keys)
+         self.player1.snake.change_direction(dir)
 
-			self.player0.player_functions()
-			self.player1.player_functions()
-			self.egg.draw()
-			
-			self.draw_area.paint_to_screen()
-			
-			# wait
-			time.sleep(0.125)
-		return GamingPage(self.screen)
+         # redraw the screen
+         self.draw_area.clear()
+
+         self.player0.player_functions()
+         self.player1.player_functions()
+         self.egg.draw()
+         
+         self.draw_area.paint_to_screen()
+         
+         # wait
+         time.sleep(self.snake_sleep)
+      return self.quit_page
 
--- Main.py
+++ Main.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python
+
 from GamingPage import *
 from SpeedMenuPage import *
 import curses.wrapper
 def test_main(screen):
-	page = GamingPage(screen)
-	#page = SpeedMenuPage(screen)
-	#draw_loop returns next page
-	#exits by no return value
-	while page != None:
-		page = page.draw_loop()
+
+   # Make getch() Non-Blocking
+   screen.nodelay(1)
+
+   #page = GamingPage(screen)
+   page = SpeedMenuPage(screen)
+   #draw_loop returns next page
+   #exits by no return value
+   while page != None:
+      page = page.draw_loop()
 
 if __name__ == "__main__":
-	curses.wrapper(test_main)
+   curses.wrapper(test_main)
